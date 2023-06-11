09632173f5313c8fe4ed03b886865b2f0f5d9d6a
Yanghwa Kim <doitykim@gmail.com>
2023-06-11 22:06:13 +0900

    Update 2023-06-11

---
+++ 3.storage/3033.add_iscsi_target.ps1
@@ -0,0 +1,18 @@
+#
+# Add an iSCSI target
+#
+
+. "$HOME/scripts/globalval.ps1"
+
+$sw_iscsi_dev = "vmhba64"
+
+foreach ($f in (import-csv -path $TgtFile))
+{
+	echo $f.alias
+	$hba = Get-VMhost $f.alias |Get-VMHostHba -Type IScsi | Where {$_.Model -eq "iSCSI Software Adapter"}
+	New-IScsiHbaTarget -IScsiHba $hba -Address $f.tgt1
+	New-IScsiHbaTarget -IScsiHba $hba -Address $f.tgt2
+	New-IScsiHbaTarget -IScsiHba $hba -Address $f.tgt3
+	New-IScsiHbaTarget -IScsiHba $hba -Address $f.tgt4
+}
+Disconnect-VIServer -Server * -Force -confirm:$false
